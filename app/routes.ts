ced71c8c736c073381874ab7821d2fd61a7b2452
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2025-01-17 13:24:20 +0545

    Employee Feature

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"), // Home route (root/index route it will be SSR)
   route("test", "routes/test.tsx"), // Test  route it will be CSR
+  route("employee","features/employee/pages/employeePage.tsx")
 ] satisfies RouteConfig;
