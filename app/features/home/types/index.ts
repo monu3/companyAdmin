10cb6abb3ba4eb532863df9780c08c5aea3baef2
Diwash <dopkhrel111@gmail.com>
2025-01-22 14:24:04 +0545

    Add MainNavbarLayoutProps and NavItem interfaces for TypeScript types

--- app/features/home/types/index.ts
+++ app/features/home/types/index.ts
@@ -3,3 +3,17 @@ export interface HomeData {
   id: number;
   name: string;
 }
+
+
+ export interface NavItem {
+  to: string;
+  label: string;
+  icon?: React.ElementType;
+}
+
+export interface MainNavbarLayoutProps {
+  items: NavItem[];
+  children: React.ReactNode;
+  logo?: string;
+  className?: string;
+}
