9be88edc5fc99911ef7e75e1ab56596a72a08cf6
Diwash <dopkhrel111@gmail.com>
2025-01-22 13:50:21 +0545

    making priority color as common component

---
+++ app/common/utils/taskPriorityColor.ts
@@ -0,0 +1,12 @@
+export const getPriorityColor = (priority: string) => {
+  switch (priority.toLowerCase()) {
+    case "high":
+      return "bg-red-100 text-red-800";
+    case "medium":
+      return "bg-yellow-100 text-yellow-800";
+    case "low":
+      return "bg-green-100 text-green-800";
+    default:
+      return "bg-gray-100 text-gray-800";
+  }
+};
