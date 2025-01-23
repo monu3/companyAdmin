6425a1326bee836915d2d4f922327e3f2e27942e
Diwash <dopkhrel111@gmail.com>
2025-01-23 12:44:07 +0545

    css is handled

--- app/features/home/components/chat.tsx
+++ app/features/home/components/chat.tsx
@@ -3,9 +3,9 @@ import { FiMessageSquare } from "react-icons/fi";
 
 const Chat = () => {
   return (
-    <div className="relative cursor-pointer hover:text-blue-600">
-      <FiMessageSquare className="text-2xl" />
-      <div className="absolute top-0 right-0 bg-red-600 text-white text-xs w-3 h-3 flex items-center justify-center rounded-full">
+    <div className=" flex  items-center relative cursor-pointer hover:text-blue-600">
+      <FiMessageSquare className="text-3xl" />
+      <div className="absolute top-0 right-0 bg-red-600 text-white text-xs w-4 h-4 flex items-center justify-center rounded-full">
         {/* Display the number of unread messages */}
       </div>
     </div>
