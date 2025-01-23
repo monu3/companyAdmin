3f7ee4eef135e37c2dac4304aecb5820d0877da5
Diwash <dopkhrel111@gmail.com>
2025-01-23 09:36:45 +0545

    Add Chat component with unread message indicator (for now it just how the structure)

--- app/features/home/components/chat.tsx
+++ app/features/home/components/chat.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { FiMessageSquare } from "react-icons/fi";
 
 const Chat = () => {
   return (
-    <div>chat</div>
-  )
-}
+    <div className="relative cursor-pointer hover:text-blue-600">
+      <FiMessageSquare className="text-2xl" />
+      <div className="absolute top-0 right-0 bg-red-600 text-white text-xs w-3 h-3 flex items-center justify-center rounded-full">
+        {/* Display the number of unread messages */}
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
