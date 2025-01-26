e6e75c6abbc5b86e5672cfc67ac3474aa1d5cc2f
Diwash <dopkhrel111@gmail.com>
2025-01-26 16:28:52 +0545

    changed the routes

--- app/routes.ts
+++ app/routes.ts
@@ -7,14 +7,15 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  index("features/signIn/signIn.tsx"),
+  route("login", "features/signIn/signIn.tsx"),
   layout("features/home/pages/HomePage.tsx", [
-    route("dashboard", "D:\companyAdmin\app\features\dashboard"),
+    index("features/dashboard/pages/dashboardPage.tsx"),
     route("projects", "features/project/pages/ProjectPage.tsx"),
     route("tasks/list", "features/task/components/listTasks.tsx"),
     route("employee", "features/employee/pages/employeePage.tsx"),
     route("tasks", "features/task/pages/taskPage.tsx"),
-    route("client","features/client/pages/clientPage.tsx")
+    route("client", "features/client/pages/clientPage.tsx"),
   ]),
-
+  route("contact","features/ContactUs/ContactUs.tsx"),
+  route("*", "features/NotFound/NotFoundPage.tsx"),
 ] satisfies RouteConfig;
