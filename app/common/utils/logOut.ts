fe9be61f45f599de36a858d18e03ff3e8634f45f
Diwash <dopkhrel111@gmail.com>
2025-01-28 21:10:36 +0545

    Implemented logout utility to clear user email from localStorage and redirect to login page.

---
+++ app/common/utils/logOut.ts
@@ -0,0 +1,12 @@
+/**
+ * logOut.ts
+ * Created On : 2025-28-01 21
+ * Author : Diwash Pokhrel
+ * Description : Utility function to log out the user by removing their email from localStorage
+ * and redirecting them to the login page.
+ */
+
+export const logout = () => {
+  localStorage.removeItem("userEmail");
+  window.location.href = "/login";
+};
