560717e4b65bee5394af078486df2b3f3d36c3c5
Diwash <dopkhrel111@gmail.com>
2025-01-28 21:03:59 +0545

    add logic for logout button

--- app/features/home/components/companyProfile.tsx
+++ app/features/home/components/companyProfile.tsx
@@ -8,12 +8,13 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { logout } from "~/common/utils/logOut";
 
 const CompanyProfile = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="bg-transparent p-0 border-none">
-        <Avatar className="inline-block size-6 flex items-center">
+        <Avatar className="inline-block size-6 items-center">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>C</AvatarFallback>
         </Avatar>
@@ -25,6 +26,14 @@ const CompanyProfile = () => {
         <DropdownMenuItem>Team</DropdownMenuItem>
         <DropdownMenuItem>Subscription</DropdownMenuItem>
         <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <button
+            onClick={logout}
+            className="w-full text-left bg-transparent p-0 border-none"
+          >
+            Logout
+          </button>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
