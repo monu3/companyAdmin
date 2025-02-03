63da656a2dee68f2e6585e74bdc503c9d8c0fbf5
Diwash <dopkhrel111@gmail.com>
2025-02-03 21:01:01 +0545

    minor change for page

--- app/features/task/Types/types.ts
+++ app/features/task/Types/types.ts
@@ -49,7 +49,7 @@ export interface Column {
  * Props for the CreateTaskForm component.
  */
 export interface CreateTaskFormProps {
-  onAddTask: (task: Task) => void; // Callback function to handle adding or updating a task
+  onAddTask: (task?: Task) => void; // Callback function to handle adding or updating a task
   selectedTask?: Task | null; // Optional: The task being edited (if in edit mode)
   setIsOpen: (isOpen: boolean) => void; // Function to control the visibility of the form modal
 }
